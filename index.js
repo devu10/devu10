a5c708cce8d2d3fd49307d6c739a16b55ef4c08b
Devu <devudevu10@gmail.com>
2024-03-24 23:28:56 +1100

    custom middleware check

--- index.js
+++ index.js
@@ -7,8 +7,27 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var passwordValid = false;
+
+function passwordCheck(req, res, next) {
+  if (req.body["password"] == "Test") {
+    passwordValid = true;
+  }
+  next();
+}
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  if (passwordValid) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
